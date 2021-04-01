03f068bf55f0d90697857b32ba285d76fa5ca9df
ericliuhusky <289471838@qq.com>
2021-04-01 19:01:48 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  title: 'EricLiu\'s Blog',
+
+  themeConfig: {
+    nav: [
+      { text: 'iOS', link: '/iOS/' },
+      { text: 'Web', link: '/Web/' },
+      { text: 'GitHub', link: 'https://github.com/ericliuhusky' },
+    ],
+
+    sidebar: {
+      '/iOS/': [
+        '',
+        'Data-structure',
+        'Algorithm'
+      ],
+      '/Web/': [
+        '',
+        'Taro'
+      ]
+    }
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+# Swift React Dart Go
+
+## Don't be addicted to technology
+
+## Products are more important than tools and programming languages
+
+## Love:two_hearts: and life are even more important
